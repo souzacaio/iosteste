df992a17ceed5be9a1cb375b99036ed377360a70
Caio <caio.souza@live.com>
2019-04-10 12:42:43 -0300

    init

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -55,7 +55,7 @@ var app = {
 
         push.on('registration', function(data) {
             console.log('registration event: ' + data.registrationId);
-            alert(data.registrationId);
+            document.getElementById('chave').value = data.registrationId;
 
             var oldRegId = localStorage.getItem('registrationId');
             if (oldRegId !== data.registrationId) {
